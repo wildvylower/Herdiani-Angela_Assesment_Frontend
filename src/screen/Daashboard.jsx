777718a72f58697198f950ee92c9a52da5e92bf2
lala <hyunie.star13@gmail.com>
2026-03-27 13:33:05 +0700

    finalee

--- src/screen/Daashboard.jsx
+++ src/screen/Daashboard.jsx
@@ -167,7 +167,7 @@ function Daashboard() {
     const handleLogout = () => {
         localStorage.clear();
         document.cookie.split(";").forEach((c) => {
-            document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/");
+            document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date(0).toUTCString() + ";path=/");
         });
         window.location.href = '/';
     };
